5fb276a3e0fdadf05c36df25f624ad1b371f2536
maciej <46327855+mwiorekk@users.noreply.github.com>
2026-05-17 21:57:54 +0200

    fix: use actually correct base path for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "/<REPO>/",
+  base: "/tw-lab5/",
 });
